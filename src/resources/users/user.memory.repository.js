f8631d8794d11bd32c50358f5ad040b8a648891e
vof <vof1111@gmail.com>
2021-05-16 23:31:01 +0300

    feat: implement /users rest endpoint

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,28 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const _ = require('lodash');
+const db = require('../../common/db');
+const User = require('./user.model');
+
+const getAll = async () => db.Users;
+
+const get = async id => {
+  const user = _.find(db.Users, ['id', id]);
+  return _.omit(user, 'password');
 };
 
-module.exports = { getAll };
+const put = async (id, putData) => {
+  const user = _.find(db.Users, ['id', id]);
+  _.assign(user, putData);
+  return _.omit(user, 'password');
+};
+
+const post = async postData => {
+  const user = new User(postData);
+  db.Users.push(user);
+  return _.omit(user, 'password');
+};
+
+const userDelete = async id => {
+  _.remove(db.Users, e => e.id === id);
+};
+
+module.exports = { getAll, get, post, put, userDelete };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,29 @@
 const router = require('express').Router();
-const User = require('./user.model');
 const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  res.json(users);
+});
+
+router.route('/').post(async (req, res) => {
+  const user = await usersService.post(req.body);
+  res.status(201).json(user);
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.get(req.params.id);
+  res.json(user);
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await usersService.put(req.params.id, req.body);
+  res.json(user);
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await usersService.userDelete(req.params.id);
+  res.json(req.params.id);
 });
 
 module.exports = router;
