49717f5ee9de739291497a4e26e5ab8ad89979ab
vof <vof1111@gmail.com>
2021-05-30 23:20:40 +0300

    feat: add boards jsdoc

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,6 +1,24 @@
 const { v4: uuid } = require('uuid');
 
+/**
+ * @typedef Board
+ * @type {object}
+ * @property {string} id - board id
+ * @property {string} title - board title
+ * @property {object[]} columns - array of columns
+ */
+
+/**
+ * Board
+ * @class Board
+ */
 class Board {
+  /**
+   * new board
+   * @param {string} [id=uuid] - board id
+   * @param {string} [title=Board1] - board title
+   * @param {object[]} [columns=[]] - columns
+   */
   constructor({ id = uuid(), title = 'Board1', columns = [] } = {}) {
     this.id = id;
     this.title = title;
