dcbc41acecbf3991b5578475ab20d0d85add34d9
vof <vof1111@gmail.com>
2021-06-27 23:38:17 +0300

    feat: add admin user creation on app start

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import { createConnection } from "typeorm";
 import { PORT } from './common/config';
 import ormconfig from "./common/ormconfig";
 import app from './app';
-import { logger } from './common/logger'
+import { logger } from './common/logger';
+import createAdmin from './utils/createAdmin';
 
-createConnection(ormconfig).then(() => {
+createConnection(ormconfig).then(async () => {
+  await createAdmin();
   app.listen(PORT, () =>
     logger.info(`App is running on http://localhost:${PORT}`)
   )
